bfddcda45a1e82d28ad24ef494a38dfe07287b38
GateQuality <ggitenzo@gmail.com>
2021-08-22 07:22:22 +0000

    add test

---
+++ server/src/database/migrations/02_create_class_schedule.test.ts
@@ -0,0 +1,7 @@
+import * as _02_create_class_schedule from "./02_create_class_schedule"
+// @ponicode
+describe("_02_create_class_schedule.up", () => {
+    test("0", async () => {
+        await _02_create_class_schedule.up(undefined)
+    })
+})
